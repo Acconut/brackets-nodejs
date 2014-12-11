397d843d5c2bd0d89138c8218efb33cc55d3d7e8
Max Graey <maxgraey@gmail.com>
2014-12-11 15:43:52 +0700

    Add v8 flags

--- preferences.js
+++ preferences.js
@@ -1,5 +1,4 @@
 define(function main(require, exports, module) {
-
     var PreferencesManager = brackets.getModule("preferences/PreferencesManager"),
         prefs = PreferencesManager.getExtensionPrefs("brackets-nodejs");
 
@@ -7,19 +6,25 @@ define(function main(require, exports, module) {
     prefs.definePreference("node-bin", "string", "");
     prefs.definePreference("npm-bin", "string", "");
     prefs.definePreference("autoscroll", "boolean", true);
+    prefs.definePreference("v8-flags", "string", "");
 
     // Conversion from the old localstorage
-    if("node-node" in localStorage) {
+    if ("node-node" in localStorage) {
         prefs.set("node-bin", localStorage["node-node"]);
         localStorage.removeItem("node-node");
     }
-    if("node-npm" in localStorage) {
+    
+    if ("node-npm" in localStorage) {
         prefs.set("npm-bin", localStorage["node-npm"]);
         localStorage.removeItem("node-npm");
+    }
+
+    if ("v8-flags" in localStorage) {
+	   prefs.set("v8-flags", localStorage["v8-flags"]);
+	   localStorage.removeItem("v8-flags");
     }
 
     prefs.save();
 
     module.exports = prefs;
-
 });
